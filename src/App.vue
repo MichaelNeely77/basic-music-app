a2bb7f4176878f25da89f6363316ff326a24e017
Michael Neely <michael@michaelneely.net>
2020-01-12 16:53:10 -0500

    Application completed

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,11 @@
         </div>
       </section>
       <section class="playlist">
-        <h3>
-          <button v-for="song in songs" :key="song.src" @click="play(song)" class="(song.src == current.src) ? 'song playing' : 'song'">
+        <h3>The Playlist</h3>
+          <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song'">
             {{ song.title }} - {{ song.artist }}
           </button>
-        </h3>
+        
       </section>
     </main>
   </div>
@@ -73,6 +73,13 @@ export default {
         this.player.src = this.current.src;
       }
       this.player.play();
+      this.player.addEventListener('ended', function() {
+        this.index++;
+        if (this.index > this.songs.length -1) {
+        this.index = 0;
+      }
+
+      }.bind(this));
       this.isPlaying = true;
     },
     pause () {
@@ -125,4 +132,103 @@ export default {
     background-color: #212121;
     color: #fff;
   }
+
+  main {
+    width: 100%;
+    max-width: 768px;
+    margin: 0 auto;
+    padding: 25px;
+  }
+
+  .song-title {
+    color: #53565a;
+    font-size: 32px;
+    font-weight: 700;
+    text-transform: uppercase;
+    text-align: center;
+  }
+  .song-title span {
+    font-weight: 400;
+    font-style: italic;
+  }
+
+  .controls {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 30px 15px;
+  }
+
+  button {
+    appearance: none;
+    background: none;
+    border: none;
+    outline: none;
+    cursor: pointer;
+  }
+
+    button:hover {
+    opacity: 0.8;
+  }
+
+  .play {
+    font-size: 20px;
+    font-weight: 700;
+    padding: 15px 25px;
+    margin: 0 15px;
+    border-radius: 8px;
+    color: #fff;
+    background-color: #cc2e5d;
+  }
+
+  .next, .prev {
+    font-size: 16px;
+    font-weight: 700;
+    padding: 10px 20px;
+    margin: 0 15px;
+    border-radius: 6px;
+    color: #fff;
+    background-color: #ff5858;
+  }
+
+  .pause {
+    font-size: 20px;
+    font-weight: 700;
+    padding: 15px 25px;
+    margin: 0 15px;
+    border-radius: 8px;
+    color: #fff;
+    background-color: blue;
+  }
+
+  .playlist {
+    padding: 0 30px;
+  }
+
+  .playlist h3 {
+    color: #212121;
+    font-size: 20px;
+    font-weight: 400;
+    margin-bottom: 30px;
+    text-align: center;
+  }
+
+  .playlist .song {
+    display: block;
+    width: 100%;
+    padding: 15px;
+    font-size: 20px;
+    font-weight: 700;
+    cursor: pointer;
+  }
+
+  .playlist .song:hover {
+    color: #ff5858;
+  }
+
+  .playlist .song.playing {
+    color: #fff;
+    background-image: linear-gradient(to right, #cc2e5d, #ff5858);
+  }
+
 </style>
